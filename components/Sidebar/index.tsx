996bb41ea4e5cbc77c2d9888057193870cb0792a
ashishmadaliya <127645762+ashishmadaliya@users.noreply.github.com>
2023-03-13 10:34:47 +0530

    sidebar ui created

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,10 +1,154 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Stack } from '@mui/system';
+import HomeIcon from '@mui/icons-material/Home';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import ProductSvg from './ProductSvg';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import EWestSvg from './EWestSvg';
+import CallIcon from '@mui/icons-material/Call';
 
+const drawerWidth = 340;
 
-const Sidebar = () => {
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  backgroundColor: '#E31E25',
+  color: 'white',
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  backgroundColor: '#E31E25',
+  color: 'white',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function MiniDrawer() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
 
-export default Sidebar
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+
+          {
+            open ?
+              <Stack>
+                <IconButton onClick={handleDrawerClose}>
+                  <ChevronLeftIcon sx={{ color: 'white' }} />
+                </IconButton>
+              </Stack>
+
+              :
+
+              <Stack>
+                <IconButton onClick={handleDrawerOpen}>
+                  <ChevronRightIcon sx={{ color: 'white' }} />
+                </IconButton>
+              </Stack>
+          }
+
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Home', 'About', 'Products', 'Applications', 'E-west Management', 'Contact Us'].map((user, index) => (
+            <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {user === 'Home' && <HomeIcon sx={{ color: 'white' }} />}
+                  {user === 'About' && <AutoStoriesIcon sx={{ color: 'white' }} />}
+                  {user === 'Products' && <ProductSvg />}
+                  {user === 'Applications' && <AppRegistrationIcon sx={{ color: 'white' }} />}
+                  {user === 'E-west Management' && <EWestSvg />}
+                  {user === 'Contact Us' && <CallIcon sx={{ color: 'white' }} />}
+
+                </ListItemIcon>
+                <ListItemText primary={user} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+              <Divider />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}

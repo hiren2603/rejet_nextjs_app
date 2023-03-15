46666824001b1523d293a2a54edd0d20405c7edc
hiren2603 <secure.hiren@gmail.com>
2023-03-15 12:57:25 +0530

    set sidebar and footer on all the pages

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,8 +4,6 @@ import Sidebar from '@/components/Sidebar'
 const About = () => {
   return (
     <>
-     <Topbar />
-      <Sidebar />
     <h2>About</h2>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Sidebar from '@/components/Sidebar';
-import { red } from '@mui/material/colors';
+import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
 import theme from "../components/Theme/Theme";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Topbar from '@/components/Topbar';
 
 
 
@@ -21,9 +15,7 @@ export default function Home() {
           <link rel="icon" href="/re.png" />
         </Head>
         <div>
-          <Topbar />
-          <Sidebar />
-          <h2>Hello Here is the entry Point of our Application</h2>
+          <h2>Home Page</h2>
         </div>
       </ThemeProvider>
     </>

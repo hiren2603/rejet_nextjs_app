fb256f9034731c894f466dd0f05cd1c25e103d5f
28Meet <90207433+28Meet@users.noreply.github.com>
2023-03-15 18:17:22 +0530

    rearrange sidebar topbar and pages

---
+++ components/Constants/index.tsx
@@ -0,0 +1,46 @@
+import { SvgIconProps } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import CallIcon from '@mui/icons-material/Call';
+import ProductSvg from "../Sidebar/ProductSvg";
+import EWestSvg from "../Sidebar/EWestSvg";
+
+export type INNER_COMPONENT_TYPE = {
+    key : string;
+    id : number;
+}[]
+
+export type MULTI_COMPONENT_TYPE = {
+    key : string;
+    id : number;
+    nestedcomponent : boolean;
+    innercomponent : INNER_COMPONENT_TYPE;
+}[]
+
+export type SIDE_BAR_ARRAY_TYPE = {
+    key : string;
+    // icon : (props: SvgIconProps) => JSX.Element;
+    icon : any;
+    id : number;
+    multicomponent : boolean;
+    multicomponents : MULTI_COMPONENT_TYPE;
+}[];
+
+export const SIDE_BAR_ARRAY : SIDE_BAR_ARRAY_TYPE = [
+    { key : "Home", icon : <HomeIcon/>, id : 1, multicomponent : false, multicomponents : []},
+    { key : "About", icon : <AutoStoriesIcon/>, id : 2, multicomponent : false, multicomponents : []},
+    { key : "Products", icon : <ProductSvg/>, id : 3, multicomponent : true, multicomponents : [
+        { key : "Inkjet", id : 7, nestedcomponent : false, innercomponent : []},
+        { key : "Laser", id : 8, nestedcomponent : true, innercomponent : [
+            { key : "CO2 Laser", id : 9},
+            { key : "Fiber Laser", id : 10},
+            { key : "UV Laser", id : 11},
+            { key : "Desktop Laser", id : 12}
+        ]},
+        {key : "Thermal", id : 13, nestedcomponent : false, innercomponent : []}
+    ] },
+    { key : "Applications", icon : <AppRegistrationIcon/>, id : 4, multicomponent : false, multicomponents : []},
+    { key : "E-west Management", icon : <EWestSvg/>, id : 5, multicomponent : false, multicomponents : []},
+    { key : "Contact", icon : <CallIcon/>, id : 6, multicomponent : false, multicomponents : []}
+]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import theme from "../components/Theme/Theme";
+import Sidebar from '@/components/Sidebar';
+import Topbar from '@/components/Topbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Topbar />
+      <Sidebar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
